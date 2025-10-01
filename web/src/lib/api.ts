c36767ff7ae2490af7e6e8174e841a4f3921398d
taiyoshimizu <taiyo.shimizu@t-suite.jp>
2025-10-02 08:50:20 +0900

    fixed api.ts

--- web/src/lib/api.ts
+++ web/src/lib/api.ts
@@ -2,3 +2,15 @@ import axios from 'axios';
 export const api = axios.create({
   baseURL: "/api",
 });
+
+api.interceptors.request.use((config) => {
+  if (config.url?.startsWith("http://localhost:4000")) {
+    config.url = config.url.replace(/^http:\/\/localhost:4000/, "");
+  }
+  if (config.baseURL?.startsWith("http://localhost:4000")) {
+    config.baseURL = "/api";
+  }
+  return config;
+});
+
+export { api }; 
