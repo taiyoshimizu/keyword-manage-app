cdae92bfa9e3f0a1fd8d9ab1e65a281a3c156f3d
taiyoshimizu <taiyo.shimizu@t-suite.jp>
2025-10-02 00:32:45 +0900

    fixed

--- web/next.config.ts
+++ web/next.config.ts
@@ -3,10 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: true
   },
   eslint: {
-    ignoreDuringBuuilds: true,
+    ignoreDuringBuuilds: true
   },
 };
 
